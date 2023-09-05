b6f32f0e3f4bcfe5deef2b1359d7b4008b39098b
Batuhan <kucukyildizbatuhan@gmail.com>
2023-09-05 21:18:35 +0300

    Initial Commit

---
+++ SwiftUI-Firebase/SwiftUI_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FirebaseApp.swift
+//  SwiftUI-Firebase
+//
+//  Created by Batuhan Küçükyıldız on 5.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
